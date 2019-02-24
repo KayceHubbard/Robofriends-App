1b473c86faa000797044ea871603935232c92a45
Kayce Hubbard <kayce.hubbard@gmail.com>
2019-02-24 15:23:24 -0600

    Updates

---
+++ precache-manifest.052845d972c5445d6072e4bce0e4fd0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-App/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1b26cdd16887e8d8b498",
+    "url": "/Robofriends-App/static/js/runtime~main.1b26cdd1.js"
+  },
+  {
+    "revision": "2a2f051e0b1e6344bee4",
+    "url": "/Robofriends-App/static/js/main.2a2f051e.chunk.js"
+  },
+  {
+    "revision": "dc5f9f00bb657253dd47",
+    "url": "/Robofriends-App/static/js/1.dc5f9f00.chunk.js"
+  },
+  {
+    "revision": "2a2f051e0b1e6344bee4",
+    "url": "/Robofriends-App/static/css/main.515c6e6b.chunk.css"
+  },
+  {
+    "revision": "dc5f9f00bb657253dd47",
+    "url": "/Robofriends-App/static/css/1.62cc86b8.chunk.css"
+  },
+  {
+    "revision": "0462ac4358af2e8793f8c228dbc219d8",
+    "url": "/Robofriends-App/index.html"
+  }
+];
